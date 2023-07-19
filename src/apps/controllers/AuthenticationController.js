56569f56bc419b48e31cf7c8a917294220943f1f
saimon rocha <saimon.rocha07@gmail.com>
2023-07-18 22:16:36 -0300

    Adicionado controller de autenticacao

---
+++ src/apps/controllers/AuthenticationController.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const Users = require('../models/Users')
+
+class AuthenticationController {
+    async authenticate(req, res) {
+        const { email, user_name, password } = req.body;
+        
+        let whereClause = {}
+        if (email) {
+            whereClause.email = { email }
+        } else if (user_name) {
+            whereClause.user_name = { user_name }
+        } else {
+            return res.status(401).json({ error: 'We need a e-mail or password' })
+        }
+
+        const verifyUser = await Users.findOne({
+            where: whereClause,
+        })
+
+        if (!verifyUser) {
+            return res.status(401).json({ error: 'User not found' })
+        }
+
+        if(!await user.checkPassword(password)){
+            return res.status(401).json({error: 'Password does not match!'})
+        }
+
+        return res.status(200).json({ message: 'User exists!' })
+    }
+}
+
+module.exports = new AuthenticationController();
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const schemaValidator = require('./apps/middleware/schemaValidator')
 
+const AuthenticationController = require('./app/controllers/AuthenticationController')
 const UserController = require('./apps/controllers/UserController')
 const userSchema = require('./schema/create.user.schema.json')
 
 const routes = new Router();
 
 routes.post('/users', schemaValidator(userSchema), UserController.create);
-
+routes.post('/auth', AuthenticationController.authenticate)
 routes.get('/health', (req, res) => {
     return res.send({ message: "Connected with sucess!" });
 });
